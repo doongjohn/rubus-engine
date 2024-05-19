2fbc5f8e3e04010a8751a3ba84b0124cc3982e07
doongjohn <doongjohn@gmail.com>
2024-05-19 15:30:01 +0900

    update

--- cmake/example.cmake
+++ cmake/example.cmake
@@ -2,17 +2,9 @@ add_executable(rubus-engine-example-game WIN32 "")
 
 set_property(TARGET rubus-engine-example-game PROPERTY EXCLUDE_FROM_ALL true)
 set_property(TARGET rubus-engine-example-game PROPERTY CXX_STANDARD 20)
-
-set(USE_ASAN false CACHE BOOL "Use AddressSanitizer")
-if (USE_ASAN)
-  set(CMAKE_CXX_FLAGS_DEBUG "-O1")
-  set_property(TARGET rubus-engine-example-game PROPERTY MSVC_RUNTIME_LIBRARY MultiThreaded)
-  set(SANITIZER_OPTS -fno-omit-frame-pointer -fno-sanitize-recover=all -fsanitize=address,undefined)
-  target_compile_options(rubus-engine-example-game PRIVATE ${SANITIZER_OPTS})
-  target_link_options(rubus-engine-example-game PRIVATE ${SANITIZER_OPTS})
-else()
-  set_property(TARGET rubus-engine-example-game PROPERTY MSVC_RUNTIME_LIBRARY MultiThreaded$<$<CONFIG:Debug>:Debug>)
-endif()
+set_property(TARGET rubus-engine-example-game PROPERTY MSVC_RUNTIME_LIBRARY MultiThreaded$<$<CONFIG:Debug>:Debug>)
+use_sanitizer(rubus-engine-example-game)
+skia_copy_icudtl_dat(rubus-engine-example-game)
 
 target_sources(
   rubus-engine-example-game
@@ -32,5 +24,3 @@ target_link_libraries(
   PRIVATE
     rubus-engine
 )
-
-skia_copy_icudtl_dat(rubus-engine-example-game)
