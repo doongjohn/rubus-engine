01369e70f445e532eb0f1fc4f24a6c65756f0aa9
doongjohn <doongjohn@gmail.com>
2024-05-24 09:54:01 +0900

    update

--- example/components.hpp
+++ example/components.hpp
@@ -4,31 +4,21 @@
 #include <rubus-engine/game/game.hpp>
 
 struct TransformComponent {
-  glm::vec3 position = glm::vec3{0, 0, 0};
-
-  static auto deinit(std::span<uint8_t>) -> void {}
+  glm::vec3 position = {0, 0, 0};
 };
 
 struct SpriteComponent {
   rugame::Sprite *sprite = nullptr;
 
-  static auto deinit(std::span<uint8_t> value) -> void {
-    auto component = reinterpret_cast<SpriteComponent *>(value.data());
-    delete component->sprite;
+  inline ~SpriteComponent() {
+    delete sprite;
   }
 };
 
-struct GreenDragonComponent {
-  static auto deinit(std::span<uint8_t>) -> void {}
-};
-
-struct RedDragonComponent {
-  static auto deinit(std::span<uint8_t>) -> void {}
-};
+struct GreenDragonComponent {};
+struct RedDragonComponent {};
 
 struct ElfWarriorComponent {
   int health = 5;
   float speed = 250.f;
-
-  static auto deinit(std::span<uint8_t>) -> void {}
 };
